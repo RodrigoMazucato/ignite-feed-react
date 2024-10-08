4a607b3f00f9df3759a59f72ada86ed5e8e9fd49
RodrigoMazucato <rodrigolopes.sp2005@gmail.com>
2024-10-08 12:44:49 -0300

    chore: criação da estrutura inicial do projeto React com Vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import styles from "./App.module.css";
+import "./global.css";
+
+function App() {
+  return (
+    <>
+      <div className={styles.wrapper}></div>
+    </>
+  );
+}
+
+export default App;
