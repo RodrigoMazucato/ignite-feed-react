8b2d6b7aa557d47ffd37ce5b0b839b5d519be586
RodrigoMazucato <rodrigolopes.sp2005@gmail.com>
2024-10-11 19:33:09 -0300

    refactor: movendo a lista de posts para um arquivo JSON

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,9 @@
 import { Header } from "./components/Header";
 import { Post } from "./components/Post";
 import { Sidebar } from "./components/Sidebar";
+import posts from "./posts.json";
 import styles from "./App.module.css";
 import "./global.css";
-
-const posts = [
-  {
-    id: 1,
-    author: {
-      avatarUrl: "https://github.com/rodrigomazucato.png",
-      name: "Rodrigo Mazucato",
-      role: "Full-Stack Developer",
-    },
-    content: [
-      { type: "paragraph", text: "Fala galeraa 👋" },
-      {
-        type: "paragraph",
-        text: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
-      },
-      { type: "link", text: "rodrigo.design/doctorcare" },
-    ],
-    publishedAt: new Date("2024-10-09 12:00"),
-  },
-  {
-    id: 2,
-    author: {
-      avatarUrl: "https://github.com/joselainejrs.png",
-      name: "Joselaine Romão",
-      role: "UX / UI Designer",
-    },
-    content: [
-      { type: "paragraph", text: "Fala galeraa 👋" },
-      {
-        type: "paragraph",
-        text: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
-      },
-      { type: "link", text: "josi.design/doctorcare" },
-    ],
-    publishedAt: new Date("2024-10-11 13:00"),
-  },
-  {
-    id: 3,
-    author: {
-      avatarUrl: "https://github.com/gustrpaz.png",
-      name: "Gustavo Rezende",
-      role: "Cloud Engineer",
-    },
-    content: [
-      { type: "paragraph", text: "Fala galeraa 👋" },
-      {
-        type: "paragraph",
-        text: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
-      },
-      { type: "link", text: "gu.design/doctorcare" },
-    ],
-    publishedAt: new Date("2024-10-11 15:00"),
-  },
-];
 
 function App() {
   return (
